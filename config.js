f83076e7a8da10536aff6fb2d1a0b23e74971b56
truong1429 <97583568+truong1429@users.noreply.github.com>
2022-01-12 13:03:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "iloveyouforever",
+    introTitle: 'Trương Thu Hà- ngố ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
