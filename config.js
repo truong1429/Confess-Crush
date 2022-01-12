7f2ef8d17906dad79e166213637aa8d3b76d6a65
truong1429 <97583568+truong1429@users.noreply.github.com>
2022-01-12 13:20:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "iloveyouforever",
     introTitle: 'Trương Thu Hà- ngố ',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    introDesc: `Người yêu của Trường không ngố từ ngày 9/7/2021`,
+    btnIntro: '^^HiHi đúng zay ^^',
+    title: ' Anh biết anh vẫn còn nhiều khuyết điểm, hay làm em buồnnn�',
+    desc: 'Nhưng em vẫn yêu anh mà nhỉiiii ',
+    btnYes: 'đúng zayy <33',
+    btnNo: 'Không nha :3-không được click',
+    question: 'anh hay làm em buồn, vì sao em vẫn yêu anh <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'vì anh không ngố và anh yêu emmmmm moah <33333333',
+    mess: 'Mình biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Cảm ơn baybe, tuần sau anh sang rùi đi chơi nghennn.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
